3ca9937eb0f4c936391c5c7636de8f0cfedc53d4
Daniel Sánchez Madrid <danielmadrid@unah.hn>
2025-10-27 18:17:44 -0600

    avances

---
+++ 04-api-todo-app/src/schemas/user.schema.ts
@@ -0,0 +1,19 @@
+import z from 'zod';
+import User  from '../interfaces/user.interface';
+
+// establecer el idioma en español
+z.config(z.locales.es());
+
+const UserSchema = z.object({
+    id: z.number().int().positive(),
+    name: z.string().min(1).max(100),
+    email: z.email()
+});
+
+export const validateUser = (user: Partial<User>) => {
+    return UserSchema.safeParse(user);
+}
+
+export const validateUserPartial = (user: Partial<User>) => {
+    return UserSchema.partial().safeParse(user);
+}
